784f6abf372cc7f9e8119fbd692591582d46ff16
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-06-06 11:46:56 +0500

    fotter responsiveness done

--- hackathon1/src/components/footer/Footer.tsx
+++ hackathon1/src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-16 border-t border-t-black flex items-center justify-around">
+    <div className="lg:h-16 border-t border-t-black flex items-center justify-evenly md:justify-around flex-col lg:flex-row py-5 lg:py-0">
       <p className="text-[#666666]">Copyright &copy; by Abu Hurairah.</p>
       <p className="text-[#666666]">
         Design by.{" "}
