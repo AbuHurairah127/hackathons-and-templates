3a7d4476c90d3f0f9762ecdb936a6246a7adb498
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-06-12 17:20:03 +0500

    DB connected with drizzle orm and working

--- hackathon1/src/app/api/(auth)/register/route.ts
+++ hackathon1/src/app/api/(auth)/register/route.ts
@@ -1,11 +1,26 @@
-import { db } from "@vercel/postgres";
+import { db, sql } from "@vercel/postgres";
 import { NextRequest, NextResponse } from "next/server";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: varchar("name"),
+  email: text("email"),
+  password: text("password"),
+});
 export async function GET() {
   try {
-    const client = await db.connect();
-    await client.sql`create table lorem(id serial primary key)`;
-    return NextResponse.json("TAble created");
+    const db = drizzle(sql);
+    const result = await db
+      .insert(users)
+      .values({
+        name: "Abu Hurairah",
+        email: "test@exp.com",
+        password: "password",
+      })
+      .returning({ insertedId: users.id, name: users.name });
+    return NextResponse.json({ result });
   } catch (error) {
     console.log(error);
   }
